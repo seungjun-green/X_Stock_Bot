b9eb56ccde30edd558b84044bf9ce091c4eb77c0
seungjun-green <lsj3285007@gmail.com>
2024-06-26 22:25:18 +0900

    final improv

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import yfinance as yf
 from datetime import datetime
 from pytz import timezone
@@ -14,23 +13,20 @@ def is_market_open():
     spy = yf.Ticker("SPY")
     data = spy.history(period="1d") 
     last_data_date = data.index.max().date()
-    today_date = datetime.now().date()
+    today_date = datetime.now(timezone('US/Eastern')).date()
     return last_data_date == today_date
-
 
 def job():
     df, date_str = get_summary()
     create_twitter_friendly_image(df, date_str)
     make_tweet(image_path="market_summary_tweet.png")
-    print(f"Tweeted - {datetime.now().date()}")
+    print(f"Tweeted - {datetime.now(timezone('US/Eastern')).date()}")
 
 
 if __name__ == "__main__":
-    print("Program Started")
+    print("Program Started\n")
     while True:
-        now = datetime.now().strftime('%H:%M')
-        if now == '16:05' and is_market_open():
+        now_est = datetime.now(timezone('US/Eastern'))
+        if now_est.strftime('%H:%M:%S') == '16:05:00' and is_market_open():
             job()
-        time.sleep(60)
-
-
+        time.sleep(1)
