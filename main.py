6bc1b44bb6a2896740c8804e7a817940a563f79f
seungjun-green <lsj3285007@gmail.com>
2024-06-26 18:03:30 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from twitter import *
+from brain import *
+
+
+df, date = get_summary()
+create_summary_image(df, date)
+
+# if __name__=="__main__": 
+#     print("Program Started")
+#     today_summary = get_summary()
+#     print(today_summary)
+#     # make_tweet(today_summary)
+    
+    
+'''
+How to Improve 
+
+1. add title 
+2. think about what information to add more and order of it
+3. add + string when change is plus
+4. think about adding $ sign
+
+
+'''
+
+
+
---
+++ twitter.py
@@ -0,0 +1,22 @@
+import tweepy
+
+consumer_key = 'mrpnK8usre8ahxxVIFs9SpGQf'
+consumer_secret = '2hMFSVggEsEKG3dL4sepdMw56XZ6U0R2uG04ej0lTB9MZBjSCO'
+access_token = '1805518108750495748-dou33BfVJCpGfVSN6iNOXFUbCmRZSR'
+access_token_secret = 'c2zC39MgG4Zxb9rItzSrH75DHwSJJJu0mT1Rmc4cpz6Lk'
+
+client = tweepy.Client(
+    consumer_key=consumer_key, 
+    consumer_secret=consumer_secret,
+    access_token=access_token, 
+    access_token_secret=access_token_secret
+)
+
+# Create the API object
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+def make_tweet(today_summary):
+    client.create_tweet(text=today_summary)
