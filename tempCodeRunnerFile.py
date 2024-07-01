4e16e31ac37b45fbf28067a53c84154037986c67
seungjun-green <lsj3285007@gmail.com>
2024-07-02 01:14:52 +0900

    ddd

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,25 +2,3 @@ import requests
 
 
     
-def get_top5_news():
-    res = ""
-    url = "https://seekingalpha.com/api/v3/news/trending"
-    headers = {
-        f'Authorization': 'Bearer {API_KEY}'
-    }
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        trending_news = response.json()
-        top_5_news = trending_news['data'][:5]  # Get the top 5 news articles
-        for idx, news in enumerate(top_5_news, 1):
-            res+=f"{idx}. {news['attributes']['title']}\n"
-            print(f"{idx}. {news['attributes']['title']}\n")
-            print(res)
-    else:
-        print(f"Failed to retrieve trending news: {response.status_code}")
-        
-    return f"Topv 5 Trending News Now: \n {res.strip()}"
-
-
-print(get_top5_news())
