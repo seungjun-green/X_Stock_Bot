d673871c933e36032873f58c14f1e6c831d3b54e
seungjun-green <lsj3285007@gmail.com>
2024-07-06 01:14:52 +0900

    fffu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 
 if __name__ == "__main__":
-    print("[Daily Motivation] Program Started\n")
+    print("[Accelerate Daily] Program Started\n")
     while True:
         now_est = datetime.now(timezone('US/Eastern'))
         if now_est.strftime('%H:%M:%S') == '22:00:00':
